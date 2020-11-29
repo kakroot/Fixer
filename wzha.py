f9ac0677c698fcc62cb8b1df7a0cd99deefbddff
kakroot <73759718+kakroot@users.noreply.github.com>
2020-11-30 02:50:49 +0300

    Add files via upload

---
+++ wzha.py
@@ -0,0 +1,142 @@
+import os 
+import time as kat
+from os import system as shell
+shell('clear')
+secuirtyname=open("wzha.py","r")
+
+fig="""
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+\033[91m
+                                           
+           '########:'####:'##::::'##:'########:'########::
+           ##.....::. ##::. ##::'##:: ##.....:: ##.... ##:
+\033[93m            ##:::::::: ##:::. ##'##::: ##::::::: ##:::: ##:
+           ######:::: ##::::. ###:::: ######::: ########::
+           ##...::::: ##:::: ## ##::: ##...:::: ##.. ##:::
+\033[92m            ##:::::::: ##::: ##:. ##:: ##::::::: ##::. ##::
+           ##:::::::'####: ##:::. ##: ########: ##:::. ##:
+           ..::::::::....::..:::::..::........::..:::::..::
+          
+\033[96m
+
+                #coder #shell
+                #snapchat : k4kshell
+                #discord : https://discord.gg/shell
+                #telegram : sh4hom 
+\033[94m
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+"""
+print(fig)
+kat.sleep(1.5)
+options="""
+\033[92m
+{❤️}1[\033[96m+\033[92m] shellphish 
+\033[91m
+{❤️}2[\033[93m+\033[91m] saycheese
+
+
+<<<<<<<<<<<<<{\033[91mteam \033[92mshell}>>>>>>>>>>>>>>>>
+
+"""
+print(options)
+
+shadow=input(" {+}\033[91m Enter number \033[92mof \033[96mthe  tool : ")
+
+if shadow==" ":
+       shell("python wzha.py")
+
+if shadow=="":
+       shell("python wzha.py")
+
+if shadow=="1":
+              print("<{ \033[92mwait \033[93mto install \033[96mshellphish }>")
+              kat.sleep(5)
+              shell("clear")
+              ban="""
+
+
+\033[92m
+.d8888. db   db d88888b db      db      d8888b. db   db d888888b .d8888. db   db 
+88'  YP 88   88 88'     88      88      88  `8D 88   88   `88'   88'  YP 88   88 
+\033[91m`8bo.   88ooo88 88ooooo 88      88      88oodD' 88ooo88    88    `8bo.   88ooo88 
+  `Y8b. 88~~~88 88~~~~~ 88      88      88~~~   88~~~88    88      `Y8b. 88~~~88 
+\033[96mdb   8D 88   88 88.     88booo. 88booo. 88      88   88   .88.   db   8D 88   88 
+`8888Y' YP   YP Y88888P Y88888P Y88888P 88      YP   YP Y888888P `8888Y' YP   YP 
+                                                                                           
+
+
+
+"""           
+              print(ban)
+              os.chdir("/data/data/com.termux/files/home")
+              shell("rm -rif shellphish")
+              print("\033[96m")
+              shell("git clone https://github.com/suljot/shellphish -l")
+              kat.sleep(2)
+              shell("rm -rif ngrok")
+              shell("clear")
+              ng="""
+               '\033[91m
+                ##::: ##::'######:::'########:::'#######::'##:::'##:
+                ###:: ##:'##... ##:: ##.... ##:'##.... ##: ##::'##::
+                ####: ##: ##:::..::: ##:::: ##: ##:::: ##: ##:'##:::
+                \033[92m## ## ##: ##::'####: ########:: ##:::: ##: #####::::
+                ##. ####: ##::: ##:: ##.. ##::: ##:::: ##: ##. ##:::
+                \033[96m##:. ###: ##::: ##:: ##::. ##:: ##:::: ##: ##:. ##::
+                ##::. ##:. ######::: ##:::. ##:. #######:: ##::. ##:
+                ..::::..:::......::::..:::::..:::.......:::..::::..::
+"""
+              print(ng)
+              shell("git clone https://github.com/kakroot/shell-ngrok -l ; cd shell-ngrok ; mv ngrok $HOME ; cd $HOME ; rm -rif shell-ngrok")
+              shell("mv ngrok shellphish")
+              shell("chmod 777 shellphish/*")
+              print("\033[92m<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<{\033[91mteam \033[93mshell>\033[92m>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+              print("\033[91m{+}  you can use \033[92mshellphish \033[91mnow \033[93mbut get new token from \033[96m{+} ngrok.com ")
+
+
+
+
+
+if shadow=="2":
+              print("<{ \033[92mwait \033[93mto install \033[96msaychees }>")
+              kat.sleep(5)
+              shell("clear")
+              ban="""
+
+
+\033[91m.d8888.  .d8b.  db    db  .o88b. db   db d88888b d88888b .d8888. d88888b 
+88'  YP d8' `8b `8b  d8' d8P  Y8 88   88 88'     88'     88'  YP 88'     
+\033[92m`8bo.   88ooo88  `8bd8'  8P      88ooo88 88ooooo 88ooooo `8bo.   88ooooo 
+  `Y8b. 88~~~88    88    8b      88~~~88 88~~~~~ 88~~~~~   `Y8b. 88~~~~~ 
+\033[96mdb   8D 88   88    88    Y8b  d8 88   88 88.     88.     db   8D 88.     
+`8888Y' YP   YP    YP     `Y88P' YP   YP Y88888P Y88888P `8888Y' Y88888P
+
+"""
+              print(ban)
+              os.chdir("/data/data/com.termux/files/home")
+              shell("rm -rif saycheese")
+              print("\033[96m")
+              shell("git clone https://github.com/vanhienfs/saycheese  -l")
+              kat.sleep(2)
+              shell("rm -rif ngrok")
+              shell("clear")
+              ng="""
+               '\033[91m
+                ##::: ##::'######:::'########:::'#######::'##:::'##:
+                ###:: ##:'##... ##:: ##.... ##:'##.... ##: ##::'##::
+                ####: ##: ##:::..::: ##:::: ##: ##:::: ##: ##:'##:::
+                \033[92m## ## ##: ##::'####: ########:: ##:::: ##: #####::::
+                ##. ####: ##::: ##:: ##.. ##::: ##:::: ##: ##. ##:::
+                \033[96m##:. ###: ##::: ##:: ##::. ##:: ##:::: ##: ##:. ##::
+                ##::. ##:. ######::: ##:::. ##:. #######:: ##::. ##:
+                ..::::..:::......::::..:::::..:::.......:::..::::..::
+"""
+              print(ng)
+              shell("git clone https://github.com/kakroot/shell-ngrok -l ; cd shell-ngrok ; mv ngrok $HOME ; cd $HOME ; rm -rif shell-ngrok")
+              shell("mv ngrok saycheese")
+              shell("chmod 777 saycheese/*")
+              print("\033[92m<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<{\033[91mteam \033[93mshell>\033[92m>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+              print("\033[91m{+}  you can use \033[92msaycheese \033[91mnow \033[93mbut get new token from \033[96m{+} ngrok.com ")
+
+if shadow > "2":
+       shell("python wzha.py")
